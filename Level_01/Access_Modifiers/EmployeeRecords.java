b0aff3ef8bee15971c716fac5c36a594dd950bec
Manish <patelmanish131149198@gmail.com>
2025-01-18 23:11:35 +0530

    Question_02

---
+++ Level_01/Access_Modifiers/EmployeeRecords.java
@@ -0,0 +1,69 @@
+// Parent class: Employee
+class Employee {
+    public int employeeID;     
+    protected String department; 
+    private double salary;  
+
+
+    public Employee(int employeeID, String department, double salary) {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    
+    public double getSalary() {
+        return salary;
+    }
+
+    
+    public void setSalary(double salary) {
+        if (salary > 0) {
+            this.salary = salary;
+            System.out.println("Updated salary: $" + salary);
+        } else {
+            System.out.println("Invalid salary amount!");
+        }
+    }
+
+   
+    public void displayEmployeeDetails() {
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: $" + salary);
+    }
+}
+
+
+class Manager extends Employee {
+    private String teamName;
+
+  
+    public Manager(int employeeID, String department, double salary, String teamName) {
+        super(employeeID, department, salary);
+        this.teamName = teamName;
+    }
+
+   
+    public void displayManagerDetails() {
+        System.out.println("\nManager Details:");
+        System.out.println("Employee ID: " + employeeID); 
+        System.out.println("Department: " + department);  
+        System.out.println("Team Name: " + teamName);
+    }
+}
+
+
+public class EmployeeRecords {
+    public static void main(String[] args) {
+    
+        Employee emp1 = new Employee(101, "IT", 60000);
+        emp1.displayEmployeeDetails();
+
+    
+        emp1.setSalary(65000);
+
+        Manager mgr1 = new Manager(201, "HR", 80000, "Recruitment Team");
+        mgr1.displayManagerDetails();
+    }
+}
